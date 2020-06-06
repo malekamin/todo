516e9be0c7dfc190a00578fba4011fe29ec55424
malekamin <66508844+malekamin@users.noreply.github.com>
2020-06-06 10:40:35 +0300

    Add files via upload

---
+++ Todo_list.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, session 
+from flask_session import Session
+
+
+
+
+app = Flask(__name__)
+
+todos = []
+
+
+@app.route("/")
+def tasks():    
+    return render_template("tasks.html" , todos=todos)
+
+    
+
+@app.route("/add", methods=["GET","POST"] )
+def add():
+    if request.method == "GET":
+         return render_template("add.html")
+    else:
+        todo = request.form.get("task")
+        todos.append(todo)
+        return redirect("/")
